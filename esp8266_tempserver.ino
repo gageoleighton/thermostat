2da1a6177564c6b640f0a932f2e02a8c65ebd916
Gage Leighton <gageoleighton@gmail.com>
2017-04-30 23:22:38 -0400

    ESP8266 Remote Temperature Sensor
    Attempt at adding support for remote temperature sensors. This server submits requests for temperature or humidity using the default adafruit script @ https://learn.adafruit.com/esp8266-temperature-slash-humidity-webserver/overview
    
    DHT library @ https://github.com/adafruit/DHT-sensor-library
    
    ESP8266WebServer library @ https://github.com/esp8266/Arduino 
    Thank you, Ian!

---
+++ esp8266_tempserver.ino
@@ -0,0 +1,116 @@
+
+/* DHTServer - ESP8266 Webserver with a DHT sensor as an input
+
+   Based on ESP8266Webserver, DHTexample, and BlinkWithoutDelay (thank you)
+
+   Version 1.0  5/3/2014  Version 1.0   Mike Barela for Adafruit Industries
+
+   Temp sensor script finalized by gageoleighton using esp8266 for Chaeron's rpi thermostat: Thank You, Chaeron!
+*/
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+// Initialize DHT sensor 
+// NOTE: For working with a faster than ATmega328p 16 MHz Arduino chip, like an ESP8266,
+// you need to increase the threshold for cycle counts considered a 1 or 0.
+// You can do this by passing a 3rd parameter for this threshold.  It's a bit
+// of fiddling to find the right value, but in general the faster the CPU the
+// higher the value.  The default for a 16mhz AVR is a value of 6.  For an
+// Arduino Due that runs at 84mhz a value of 30 works.
+// This is for the ESP8266 processor on ESP-01 
+DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
+
+const char* ssid     = "WiFi Name";
+const char* password = "WiFi Password";
+
+ESP8266WebServer server(80);
+
+float humidity, temperature;  // Values read from sensor
+String webString="";     // String to display
+ 
+void handle_root() {
+  server.send(200, "text/plain", "Hello from the weather esp8266, read from direcories /temp or /humidity");
+  delay(100);
+}
+
+unsigned long previousMillis = 0;
+unsigned long interval = 2000;
+
+void setup(void)
+{
+  dht.begin();
+  
+  // You can open the Arduino IDE Serial Monitor window to see what the code is doing
+  Serial.begin(115200);  // Serial connection from ESP-01 via 3.3v console cable
+//  dht.begin();           // initialize temperature sensor
+
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+  Serial.print("\n\r \n\rWorking to connect");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("DHT Weather Reading Server");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+   
+  server.on("/", handle_root);
+  
+  server.on("/temp", [](){  // if you add this subdirectory to your webserver call, you get text below :)
+    gettemperature();       // read sensor
+    Serial.println(temperature);
+    webString=String(temperature, 2);   // Arduino has a hard time with float to string
+    server.send(200, "text/plain", webString);            // send to someones browser when asked
+  });
+
+  server.on("/humidity", [](){  // if you add this subdirectory to your webserver call, you get text below :)
+    gettemperature();       // read sensor    
+    Serial.println(humidity);
+    webString=String(humidity, 1);
+    server.send(200, "text/plain", webString);               // send to someones browser when asked
+  });
+  
+  server.begin();
+  Serial.println("HTTP server started");
+
+
+}
+ 
+void loop(void)
+{
+  server.handleClient();
+} 
+
+void gettemperature() {
+  // Wait at least 2 seconds seconds between measurements.
+  // if the difference between the current time and last time you read
+  // the sensor is bigger than the interval you set, read the sensor
+  // Works better than delay for things happening elsewhere also
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis >= interval) {
+    // save the last time you read the sensor 
+    previousMillis = currentMillis;   
+
+    // Reading temperature for humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+    humidity = dht.readHumidity();          // Read humidity (percent)
+    temperature = dht.readTemperature(true);     // Read temperature as Fahrenheit if you want celcius just remove 'true'
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(temperature)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+  }
+}
